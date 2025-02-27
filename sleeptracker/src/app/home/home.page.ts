22e31593f02d820056f031b460b7bda2b0c81cb9
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-02-27 08:34:33 -0800

    setup app & home page with basic background

--- sleeptracker/src/app/home/home.page.ts
+++ sleeptracker/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { SleepService } from '../services/sleep.service';
+import { DatePipe } from '@angular/common';
 import { SleepData } from '../data/sleep-data';
 import { OvernightSleepData } from '../data/overnight-sleep-data';
 import { StanfordSleepinessData } from '../data/stanford-sleepiness-data';
@@ -9,16 +10,40 @@ import { StanfordSleepinessData } from '../data/stanford-sleepiness-data';
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  providers: [DatePipe],
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true
 })
 export class HomePage {
-  constructor(public sleepService:SleepService) {
+  currentTime: string = '';
+
+  constructor(public sleepService:SleepService, private datePipe: DatePipe) {
 
 	}
 
 	ngOnInit() {
 		console.log(this.allSleepData);
+    this.updateTime();
+    setInterval(() => {
+      this.updateTime();
+    }, 1000);
 	}
+
+  updateTime() {
+    const now = new Date();
+    this.currentTime = this.datePipe.transform(now, 'shortTime') || '';
+  }
+
+  get greeting(): string {
+    const hour = new Date().getHours();
+    if (hour < 12) {
+      return 'Good Morning';
+    } else if (hour < 18) {
+      return 'Good Afternoon';
+    } else {
+      return 'Good Night';
+    }
+  }
 
 	/* Ionic doesn't allow bindings to static variables, so this getter can be used instead. */
 	get allSleepData() {
